291f89cdc00aec9c8ba2630a28db55057e36ec5d
peuchen <npeuchen@gmail.com>
2020-11-02 19:53:47 +0100

    Add pseudocode

---
+++ mastermind.rb
@@ -0,0 +1,9 @@
+#Create a Game class
+#Initialize a new game by setting up a board containing a random code
+#Create a play method to start the game
+
+#Create a Player class
+#Create a method for the player to enter a guess
+#Create a method that checks the guess
+  #If true: Print the random code and congratulate the player
+  #If false: Give feedback on the guess
