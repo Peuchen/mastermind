d30036fb42dddf7a74e90a51fd758a8d524ea004
peuchen <npeuchen@gmail.com>
2020-11-05 10:59:10 +0100

    Set up Game and Player classes

--- mastermind.rb
+++ mastermind.rb
@@ -1,9 +1,21 @@
 #Create a Game class
+class Game
 #Initialize a new game by setting up a board containing a random code
+  def initialize
+  end
 #Create a play method to start the game
+  def play
+  end
+end
 
 #Create a Player class
+class Player
 #Create a method for the player to enter a guess
+  def guess
+  end
 #Create a method that checks the guess
+  def is_correct?
   #If true: Print the random code and congratulate the player
   #If false: Give feedback on the guess
+  end
+end
