655bf56a3a336f6eedd3a50ce7f948a20564c053
peuchen <npeuchen@gmail.com>
2020-11-05 17:03:31 +0100

    Add input length check to guess method

--- mastermind.rb
+++ mastermind.rb
@@ -12,7 +12,9 @@ public
 def play
     puts "The computer has generated a secret code."
     player = Player.new
-    12.times do
+    #2 instead of 12 for test purposes
+    2.times do |turn|
+      puts "---\nTurn #{turn+1}\n---"
       check_guess(player.guess)
       break if $win === true
     end
@@ -52,16 +54,16 @@ end
 
 #Create a Player class
 class Player
-  attr_reader :guess
+  attr_reader :input
 #Create a method for the player to enter a guess
   def guess
     puts "Please enter your guess, consisting of 4 numbers between 0 and 5."
-    input = gets.chomp
-    until /([0-5][0-5][0-5][0-5])/.match?(input)
+    @input = gets.chomp
+    until input.length === 4 && /([0-5][0-5][0-5][0-5])/.match?(input)
       puts "Your input does not consist of 4 numbers between 0 and 5. Please try again."
-      input = gets.chomp
+      @input = gets.chomp
     end
-    input
+    @input
   end
 end
 
