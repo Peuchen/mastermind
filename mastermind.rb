6083455a28c22c6ad639e95c867545c2c6356409
peuchen <npeuchen@gmail.com>
2020-11-05 11:24:39 +0100

    Set up initialize and play methods for Game class

--- mastermind.rb
+++ mastermind.rb
@@ -2,9 +2,21 @@
 class Game
 #Initialize a new game by setting up a board containing a random code
   def initialize
+    protected
+    @code = ""
+    4.times {@code += rand(6).to_s}
   end
 #Create a play method to start the game
   def play
+    puts "The computer has generated a secret code. Please enter your first guess."
+    player = Player.new
+    self.check_guess(player.guess)
+  end
+
+  #Create a method that checks the guess
+  def check_guess
+    #If true: Print the random code and congratulate the player
+    #If false: Give feedback on the guess
   end
 end
 
@@ -13,9 +25,5 @@ class Player
 #Create a method for the player to enter a guess
   def guess
   end
-#Create a method that checks the guess
-  def is_correct?
-  #If true: Print the random code and congratulate the player
-  #If false: Give feedback on the guess
-  end
+
 end
